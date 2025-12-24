9ef35f54e28633b95ed0489c951f6c3b36be7672
TonmoyZohani <tonmoyzohani@gmail.com>
2025-12-25 01:42:30 +0600

    create operation done

---
+++ crud.ts
@@ -0,0 +1,35 @@
+import { prisma } from "./lib/prisma";
+
+async function run() {
+  //   const createUser = await prisma.user.create({
+  //     data: {
+  //       name: "Rahim",
+  //       email: "rahim@gmail.com",
+  //       role: "User",
+  //     },
+  //   });
+
+//   const createPost = await prisma.post.create({
+//     data: {
+//       title: "This is a post",
+//       content: "This is the content of the post",
+//       isPublished: true,
+//       authorId: 1,
+//     },
+//   });
+
+const createProfile = await prisma.profile.create({
+  data: {
+    bio: "This is my bio",
+    dateOfBirth: new Date("1990-01-01"),
+    userId: 1,
+  },
+});
+
+  //   console.log("Create User:", createUser);
+
+//   console.log("Create Post:", createPost);
+console.log("Create Profile:", createProfile);
+}
+
+run();
