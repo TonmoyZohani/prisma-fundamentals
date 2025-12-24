232502f17d20e4fe8f13962abe4358f4877cdb0e
TonmoyZohani <tonmoyzohani@gmail.com>
2025-12-25 01:52:14 +0600

    read done

--- crud.ts
+++ crud.ts
@@ -9,27 +9,45 @@ async function run() {
   //     },
   //   });
 
-//   const createPost = await prisma.post.create({
-//     data: {
-//       title: "This is a post",
-//       content: "This is the content of the post",
-//       isPublished: true,
-//       authorId: 1,
-//     },
-//   });
+  //   const createPost = await prisma.post.create({
+  //     data: {
+  //       title: "This is a post",
+  //       content: "This is the content of the post",
+  //       isPublished: true,
+  //       authorId: 1,
+  //     },
+  //   });
 
-const createProfile = await prisma.profile.create({
-  data: {
-    bio: "This is my bio",
-    dateOfBirth: new Date("1990-01-01"),
-    userId: 1,
-  },
-});
+  // const createProfile = await prisma.profile.create({
+  //   data: {
+  //     bio: "This is my bio",
+  //     dateOfBirth: new Date("1990-01-01"),
+  //     userId: 1,
+  //   },
+  // });
 
   //   console.log("Create User:", createUser);
 
-//   console.log("Create Post:", createPost);
-console.log("Create Profile:", createProfile);
+  //   console.log("Create Post:", createPost);
+  // console.log("Create Profile:", createProfile);
+
+  const users = await prisma.user.findMany({
+    // include: {
+    //   posts: true,
+    //   profile: true,
+    // },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      role: true,
+      posts: true,
+      profile: true,
+    },
+  });
+
+  //   console.log("Users", users);
+  console.dir(users, { depth: Infinity });
 }
 
 run();
